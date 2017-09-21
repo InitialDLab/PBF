7acb469414d185e58f966ba748ac357cba1e780f
Jinwei Guo <guojinwei@stu.ecnu.edu.cn>
2017-09-21 17:20:37 +0800

    first

---
+++ src/main/java/edu/ecnu/pbf/PbfMain.java
@@ -0,0 +1,13 @@
+package edu.ecnu.pbf;
+
+/**
+ * Hello world!
+ *
+ */
+public class PbfMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
