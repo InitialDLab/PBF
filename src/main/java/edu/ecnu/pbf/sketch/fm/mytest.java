8179cb94d2ffdd36aac7001f479b16a34758715d
Yanqing Peng <yqpeng@foxmail.com>
2017-10-14 01:13:35 -0600

    update

---
+++ src/main/java/edu/ecnu/pbf/sketch/fm/mytest.java
@@ -0,0 +1,17 @@
+package edu.ecnu.pbf.sketch.fm;
+
+public class mytest {
+
+	public static void main(String[] args) {
+
+	    HyperLogLog hll = HyperLogLog.builder().build();
+	    for (int i = 0; i < 200000; i++) {
+	    	for (int j = 0; j < 3; j++) {
+	    		hll.addString(i+"i");
+	    	}
+	    }
+	    System.out.println("estimate count: " + hll.count());
+	}
+
+}
+
